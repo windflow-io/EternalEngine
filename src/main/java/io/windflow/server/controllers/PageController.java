0cebee5945fc8b8fbdc40899852767d641a33379
Mark van Wyk <mark@flyh2.com>
2020-06-30 21:13:16 +0200

    Updated the PageController to allow subfolders /this/that/whatever/ (.json)

--- src/main/java/io/windflow/server/controllers/PageController.java
+++ src/main/java/io/windflow/server/controllers/PageController.java
@@ -1,36 +1,51 @@
 package io.windflow.server.controllers;
 
 import io.windflow.server.StubReader;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UncheckedIOException;
 
 @RestController
 public class PageController {
 
-    @RequestMapping(value = {"/api/pages/{host}/{urlPath}", "/api/pages/{host}"}, produces = "application/json")
+    @RequestMapping(value = {"/api/pages/**"}, produces = "application/json")
     @ResponseBody
-    public String mjs(@PathVariable("host") String host, @PathVariable(value = "urlPath", required = false) String urlPath, HttpServletResponse response) {
-        System.out.println(urlPath);
-        urlPath = (urlPath == null ? "/index" : "/" + urlPath);
-        host = !host.contains(":") ? host : host.substring(0, host.indexOf(":"));
-        String filePath = "/stubs/pages/" + host + urlPath + ".json";
+    public String servePage(HttpServletRequest request, HttpServletResponse response) {
 
-        System.out.println(filePath);
+        RequestParser parser = new RequestParser(request);
 
         try {
-            return StubReader.loadStub(filePath);
+            return StubReader.loadStub(parser.getFilePath());
         } catch (UncheckedIOException ex) {
             System.err.println("WINDFLOW ERROR: " + ex.getMessage());
-            ex.printStackTrace();
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return StubReader.loadStub("/stubs/pages/windflowx/404.json");
         }
     }
-}
 
+    private static class RequestParser {
+
+        private HttpServletRequest request;
+        private String filePath;
+
+        RequestParser(HttpServletRequest request) {
+            String requestedPath = request.getRequestURI().replace("/api/pages", "");
+            String hostAndPort = requestedPath.split("/")[1];
+            String urlPath = requestedPath.replace("/" + hostAndPort + "/", "");
+            urlPath = urlPath.endsWith("/") ? urlPath.substring(0, urlPath.length() -1) : urlPath;
+            String host = !hostAndPort.contains(":") ? hostAndPort : hostAndPort.substring(0, hostAndPort.indexOf(":"));
+            urlPath = (urlPath.length() == 0 ? "/index" : "/" + urlPath);
+            this.filePath = "/stubs/pages/" + host + urlPath + ".json";
+        }
+
+        public String getFilePath() {
+            return this.filePath;
+        }
+
+    }
+}
