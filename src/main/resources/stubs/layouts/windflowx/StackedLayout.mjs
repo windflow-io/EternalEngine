bbe7955eba6629474e7a3b8d60702e9a278cff39
Mark van Wyk <mark@flyh2.com>
2020-07-02 22:37:17 +0200

    Themes solved

--- src/main/resources/stubs/layouts/windflowx/StackedLayout.mjs
+++ src/main/resources/stubs/layouts/windflowx/StackedLayout.mjs
@@ -3,7 +3,13 @@ export default {
     name: 'StackedLayout',
     components: {FlowArea},
     template:
-        `<section class="flex flex-row h-screen bg-gray-900">
-            <div class="flex-grow bg-blue-500 bg-opacity-25 p-1 text-white"><flow-area name="main"></flow-area></div>
+        `<section class="flex flex-row h-screen" :class="[backgroundColor, foregroundColor]" >
+            <div class="flex-grow bg-blue-500 bg-opacity-25 p-1"><flow-area name="main"></flow-area></div>
         </section>`,
+    data() {
+        return {
+            backgroundColor: Math.round(Math.random()) === 1 ? 'bg-gray-900' : 'bg-red-900',
+            foregroundColor: Math.round(Math.random()) === 1 ? 'text-yellow-500' : 'text-green-500'
+        }
+    }
 }
