5f5e27288fb70eb05fcb1f35280f75a94c564404
Mark van Wyk <mark@flyh2.com>
2020-07-04 20:31:39 +0200

    Removed the datasource stuff completely

--- src/main/java/io/windflow/server/WindflowServer.java
+++ src/main/java/io/windflow/server/WindflowServer.java
@@ -2,6 +2,7 @@ package io.windflow.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class WindflowServer {
