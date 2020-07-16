ea1c0602e1fcd44b0e65d1cb50f866878c417eab
Mark van Wyk <mark@flyh2.com>
2020-07-16 20:09:30 +0200

    Testing JRebel with Graal

--- src/main/resources/stubs/layouts/com.mimo-connect.app/DashboardLayout.mjs
+++ src/main/resources/stubs/layouts/com.mimo-connect.app/DashboardLayout.mjs
@@ -6,9 +6,9 @@ export default {
     template:
         `<section>
             <div class="w-full flex flex-row">
-                <div class="flex-grow bg-red-100">1!</div>
-                <div class="flex-grow bg-blue-100">2!</div>
-                <div class="flex-grow bg-purple-100">3!</div>               
+                <div class="flex-grow bg-red-100">1*</div>
+                <div class="flex-grow bg-blue-100">2*</div>
+                <div class="flex-grow bg-purple-100">3*</div>               
             </div>
             <div class=""></div>
         </section>                 
