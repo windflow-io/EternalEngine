bee723bea87e12f598703d46aec1ad7b2086e757
Mark van Wyk <mark@flyh2.com>
2020-10-27 16:15:19 +0200

    404 stuff pretty mature

--- src/main/resources/data/system.windflow.io/components/NotFoundDisplay.js
+++ src/main/resources/data/system.windflow.io/components/NotFoundDisplay.js
@@ -5,7 +5,7 @@ export default {
         <section class="text-center">
             <h1 class="text-6xl text-white">{{errorTitle}}</h1>
             <h2 class="text-3xl text-gray-400 -mt-4">{{errorDescription}}</h2>
-            <h2 class="text-base text-gray-600 mt-2">{{errorDetail}}</h2>
+            <h3 class="text-base text-gray-600 mt-2">{{errorDetail}}</h3>
         </section>
         `
 }
